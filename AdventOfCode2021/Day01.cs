b5111815d6d3077f65f7b0869c42b14fbf9508ed
Rick Gray <rickdgray@outlook.com>
2021-11-30 23:33:41 -0600

    day 1

--- AdventOfCode2021/Day01.cs
+++ AdventOfCode2021/Day01.cs
@@ -4,12 +4,38 @@
     {
         public static long Part1(List<string> data)
         {
-            return -1;
+            var count = 0;
+            for (var i = 1; i < data.Count; i++)
+            {
+                int.TryParse(data[i-1], out var lower);
+                int.TryParse(data[i], out var higher);
+
+                if (higher > lower)
+                {
+                    count++;
+                }
+            }
+
+            return count;
         }
 
         public static long Part2(List<string> data)
         {
-            return -1;
+            var count = 0;
+            for (var i = 3; i < data.Count; i++)
+            {
+                int.TryParse(data[i - 3], out var first);
+                int.TryParse(data[i - 2], out var second);
+                int.TryParse(data[i - 1], out var third);
+                int.TryParse(data[i], out var forth);
+
+                if (forth + third + second > third + second + first)
+                {
+                    count++;
+                }
+            }
+
+            return count;
         }
     }
 }
