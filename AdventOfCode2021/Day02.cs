462e29d4982fd46f932e433d6e2d76a64665b26f
Rick Gray <rickdgray@outlook.com>
2021-12-01 23:12:28 -0600

    day 2

--- AdventOfCode2021/Day02.cs
+++ AdventOfCode2021/Day02.cs
@@ -4,12 +4,56 @@
     {
         public static long Part1(List<string> data)
         {
-            return -1;
+            var x = 0;
+            var y = 0;
+            foreach (var line in data)
+            {
+                var instruction = line.Split(" ")[0];
+                var distance = int.Parse(line.Split(" ")[1]);
+
+                switch (instruction)
+                {
+                    case "forward":
+                        x += distance;
+                        break;
+                    case "up":
+                        y -= distance;
+                        break;
+                    case "down":
+                        y += distance;
+                        break;
+                }
+            }
+
+            return x * y;
         }
 
         public static long Part2(List<string> data)
         {
-            return -1;
+            var aim = 0;
+            var x = 0;
+            var y = 0;
+            foreach (var line in data)
+            {
+                var instruction = line.Split(" ")[0];
+                var value = int.Parse(line.Split(" ")[1]);
+
+                switch (instruction)
+                {
+                    case "forward":
+                        x += value;
+                        y += aim * value;
+                        break;
+                    case "up":
+                        aim -= value;
+                        break;
+                    case "down":
+                        aim += value;
+                        break;
+                }
+            }
+
+            return x * y;
         }
     }
 }
